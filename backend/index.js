f859ada82fe9364e0021feadfdfbe70dfc474f46
San Shwe <sanxhwe@gmail.com>
2024-06-18 11:33:38 +0630

    Open AI

--- backend/index.js
+++ backend/index.js
@@ -3,30 +3,54 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const axios = require("axios");
+const { OpenAI } = require("openai");
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const PORT = process.env.PORT || 3001;
-const QUOTABLE_API_URL = "https://api.quotable.io/random";
 
-const generateQuote = async () => {
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
+});
+
+async function generateQuote() {
   try {
-    const response = await axios.get(QUOTABLE_API_URL);
-    if (response.status === 200) {
-      const { content, author } = response.data;
-      console.log("Quote generated:", content, "-", author);
-      return `${content} - ${author}`;
-    } else {
-      console.error("Unexpected response status:", response.status);
-      return "Unexpected response status.";
-    }
+    const params = {
+      messages: [{ role: "user", content: "Say this is a test" }],
+      model: "gpt-3.5-turbo",
+    };
+
+    const chat = await openai.chat.completions.create(params);
+
+    const message = chat.choices[0].message.content;
+
+    return message;
   } catch (error) {
     console.error("Error generating quote:", error.message);
-    return "Sorry, I couldn't generate a quote.";
+    return "Sorry, I couldn't generate a text.";
   }
-};
+}
+
+// const QUOTABLE_API_URL = "https://api.quotable.io/random";
+
+// const generateQuote = async () => {
+//   try {
+//     const response = await axios.get(QUOTABLE_API_URL);
+//     if (response.status === 200) {
+//       const { content, author } = response.data;
+//       console.log("Quote generated:", content, "-", author);
+//       return `${content} - ${author}`;
+//     } else {
+//       console.error("Unexpected response status:", response.status);
+//       return "Unexpected response status.";
+//     }
+//   } catch (error) {
+//     console.error("Error generating quote:", error.message);
+//     return "Sorry, I couldn't generate a quote.";
+//   }
+// };
 
 wss.on("connection", (ws) => {
   console.log("A user connected");
